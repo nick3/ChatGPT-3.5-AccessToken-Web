6e780848988575bc559de05bbef140e770332aac
Nick Liu <imjustanemail@gmail.com>
2023-09-27 16:04:31 +0800

    支持使用refresh_token来获取access_token

--- myscript.sh
+++ myscript.sh
@@ -1,3 +1,3 @@
 python /app/ChatGPT-3.5-AccessToken-Web/autorestart.py
 cd /app/pandora
-nohup pandora -s 0.0.0.0:8008 -t token.txt > output.log 2>&1 &
+nohup pandora -s 0.0.0.0:8008 -t /config/token.txt > output.log 2>&1 &
