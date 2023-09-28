b1f0b4049d9b544d6e407e3db9df82701de9fbd8
Nick Liu <imjustanemail@gmail.com>
2023-09-28 11:00:18 +0800

    优化代码

--- auto_pool_token.py
+++ auto_pool_token.py
@@ -1,140 +1,108 @@
 # -*- coding: utf-8 -*-
 
 import os
-from os import path
-from certifi import where
 import json
 import requests
-
+from certifi import where
 from pandora.openai.auth import Auth0
 
+# 获取访问令牌的 URL
+ACCESS_TOKEN_URL = 'https://auth0.openai.com/oauth/token'
+# 伪装的用户代理
+USER_AGENT = ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
+              'Chrome/109.0.0.0 Safari/537.36')
+
+# 请求参数
+REQ_KWARGS = {
+    'verify': where(),
+    'timeout': 100,
+}
 
 def parse_access_token(resp):
+    """
+    解析获取到的访问令牌。
+    """
     if resp.status_code == 200:
-        json = resp.json()
-        if 'access_token' in json:
-            return json['access_token']
+        response_json = resp.json()
+        if 'access_token' in response_json:
+            return response_json['access_token']
         else:
             raise Exception('Get access token failed.')
     else:
         raise Exception(resp.text)
-    
-def get_access_token_by_refresh_token(refresh_token: str) -> str:
-    url = 'https://auth0.openai.com/oauth/token'
-    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) ' \
-                          'Chrome/109.0.0.0 Safari/537.36'
-    headers = {
-        'User-Agent': user_agent,
-    }
+
+def login_with_refresh_token(refresh_token):
+    """
+    使用刷新令牌登录。
+    """
+    headers = {'User-Agent': USER_AGENT}
     data = {
         "redirect_uri": "com.openai.chat://auth0.openai.com/ios/com.openai.chat/callback",
         "grant_type": "refresh_token",
         "client_id": "pdlLIX2Y72MIl2rhLhTE9VV9bN905kBh",
         "refresh_token": refresh_token
     }
-    req_kwargs = {
-        'verify': where(),
-        'timeout': 100,
-    }
-    session = requests.Session()
-    resp = session.post(url=url, headers=headers, data=data, allow_redirects=False, **req_kwargs)
-
+    resp = requests.post(url=ACCESS_TOKEN_URL, headers=headers, data=data, allow_redirects=False, **REQ_KWARGS)
     return parse_access_token(resp)
 
-def run():
-    proxy = None
-    expires_in = 0
-    unique_name = 'my share token'
-    current_path = os.getcwd()  # 获取当前工作目录路径
-    parent_path = os.path.dirname(current_path)  # 获取父目录路径
-    current_dir = path.dirname(path.abspath(__file__))
-    credentials_file = '/config/users.txt'
-    # credentials_file = path.join(current_dir, 'user.txt')
+def login_with_password(username, password, proxy):
+    """
+    使用用户名和密码登录。
+    """
+    return Auth0(username, password, proxy).auth()
 
+def handle_login(credential):
+    """
+    处理登录逻辑。
+    """
+    token_info = {
+        'username': credential[0].strip(),
+        'token': 'None',
+        'share_token': 'None',
+    }
+
+    try:
+        if len(credential) == 3:
+            # 使用刷新令牌登录
+            print('Login begin with refresh token: {}'.format(token_info['username']))
+            token_info['token'] = login_with_refresh_token(credential[2].strip())
+        elif len(credential) == 2:
+            # 使用用户名和密码登录
+            print('Login begin with username/password: {}'.format(token_info['username']))
+            token_info['token'] = login_with_password(credential[0].strip(), credential[1].strip(), None)
+    except Exception as e:
+        err_str = str(e).replace('\n', '').replace('\r', '').strip()
+        print('Login failed: {}, {}'.format(token_info['username'], err_str))
+        token_info['token'] = err_str
+
+    return token_info
+
+def run():
+    # current_dir = os.path.dirname(os.path.abspath(__file__))
+    # credentials_file = os.path.join(current_dir, 'config', 'users.txt')
+    # tokens_file = os.path.join(current_dir, 'config', 'tokens.txt')
+    credentials_file = '/config/users.txt'
     tokens_file = '/config/tokens.txt'
-    # tokens_file = path.join(current_dir, 'token.txt')
 
     with open(credentials_file, 'r', encoding='utf-8') as f:
-        credentials = f.read().split('\n')
-    credentials = [credential.split(',', 2) for credential in credentials]
-    print(credentials)
+        credentials = [credential.split(',', 2) for credential in f.read().split('\n') if credential]
 
-    count = 0
     token_keys = []
-    for credential in credentials:
-        progress = '{}/{}'.format(credentials.index(credential) + 1, len(credentials))
-        count += 1
-        
-        if not credential:
-            continue
-        
-        if len(credential) == 3:
-            username, refresh_token = credential[0].strip(), credential[2].strip()
-            print('Login begin: {}, {}'.format(username, progress))
-            print(refresh_token)
-
-            token_info = {
-                'username': username,
-                'token': 'None',
-                'share_token': 'None',
-            }
-            token_keys.append(token_info)
-
-            try:
-                token_info['token'] = get_access_token_by_refresh_token(refresh_token)
-                print('Login success: {}, {}'.format(username, progress))
-            except Exception as e:
-                err_str = str(e).replace('\n', '').replace('\r', '').strip()
-                print('Login failed: {}, {}'.format(username, err_str))
-                token_info['token'] = err_str
-                continue
-                
-        elif len(credential) == 2:
-            username, password = credential[0].strip(), credential[1].strip()
-            print('Login begin: {}, {}'.format(username, progress))
-            print(password)
-
-            token_info = {
-                'username': username,
-                'token': 'None',
-                'share_token': 'None',
-            }
-            token_keys.append(token_info)
-
-            try:
-                token_info['token'] = Auth0(username, password, proxy).auth()
-                print('Login success: {}, {}'.format(username, progress))
-            except Exception as e:
-                err_str = str(e).replace('\n', '').replace('\r', '').strip()
-                print('Login failed: {}, {}'.format(username, err_str))
-                token_info['token'] = err_str
-                continue
-
-        # data = {
-        #     'unique_name': unique_name,
-        #     'access_token': token_info['token'],
-        #     'expires_in': expires_in,
-        # }
-        # resp = requests.post('https://ai.fakeopen.com/token/register', data=data)
-        # if resp.status_code == 200:
-        #     token_info['share_token'] = resp.json()['token_key']
-        #     print('share token: {}'.format(token_info['share_token']))
-        # else:
-        #     err_str = resp.text.replace('\n', '').replace('\r', '').strip()
-        #     print('share token failed: {}'.format(err_str))
-        #     token_info['share_token'] = err_str
-        #     continue
-
-    # with open(tokens_file, 'w', encoding='utf-8') as f:
-    #     for token_info in token_keys:
-    #         f.write('{}\n'.format(token_info['token']))
-
+    for idx, credential in enumerate(credentials):
+        progress = '{}/{}'.format(idx + 1, len(credentials))
+        token_info = {
+            'username': credential[0].strip(),
+            'token': 'None',
+            'share_token': 'None',
+        }
+        print('Login: {}, {}'.format(token_info['username'], progress))
+        token_info = handle_login(credential)
+        token_keys.append(token_info)
 
     with open(tokens_file, 'w', encoding='utf-8') as f:
         data = {token_info['username']: token_info['token'] for token_info in token_keys}
         json_data = json.dumps(data)
         f.write(json_data)
-
 
 if __name__ == '__main__':
     run()
