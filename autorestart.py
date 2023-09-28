6f5e06909aec54f605406e5dd95e033ce2117db6
Nick Liu <imjustanemail@gmail.com>
2023-09-28 10:11:24 +0800

    自动重启脚本不升级pandora

--- autorestart.py
+++ autorestart.py
@@ -1,15 +1,15 @@
 import os
 
 def restart():
-    # 执行pip3 install命令
+    # 执行 pip3 install 命令
     os.system('kill -9 $(lsof -t -i:8008)')
 
-    os.system('pip3 install --upgrade pandora-chatgpt')
+    # os.system('pip3 install --upgrade pandora-chatgpt')
     
-    # 执行python3 auto_pool_token.py命令
+    # 执行 python3 auto_pool_token.py 命令
     os.system('python3 /app/ChatGPT-3.5-AccessToken-Web/auto_pool_token.py')
     # os.system('cd /app/pandora')
-    # 执行yarn pandora -s -t tokens.txt &命令
+    # 执行 yarn pandora -s -t tokens.txt &命令
     # os.system('nohup pandora -s 0.0.0.0:8008 -t token.txt > output.log 2>&1 &')
     
     print('Commands executed successfully!')
